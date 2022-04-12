624818cb4140d209f1af15b713bea4e0fe2519c4
thiagopcdev <thiagopc_dev@hotmail.com>
2022-04-12 14:39:27 -0300

    Implements index to consume the car class

---
+++ index.ts
@@ -0,0 +1,41 @@
+import Car from "./Car";
+import Colors from "./Colors";
+import Directions from "./Directions";
+import Doors from "./Doors";
+
+
+const gol = new Car("Fiat", Colors.BLACK, 4);
+
+gol.openTheDoor(Doors.DRIVER);
+gol.closeTheDoor(Doors.DRIVER);
+gol.turnOn();
+gol.speedUp();
+gol.speedUp();
+gol.speedDown();
+gol.turn(Directions.LEFT);
+gol.speedUp();
+gol.speedDown();
+gol.turn(Directions.RIGHT);
+gol.speedUp();
+gol.speedDown();
+gol.turn(Directions.RIGHT);
+gol.speedUp();
+gol.speedDown();
+gol.stop();
+gol.openTheDoor(Doors.BEHIND_RIDE);
+gol.closeTheDoor(Doors.BEHIND_RIDE);
+gol.speedUp();
+gol.speedUp();
+gol.speedDown();
+gol.turn(Directions.RIGHT);
+gol.speedUp();
+gol.speedDown();
+gol.turn(Directions.LEFT);
+gol.speedUp();
+gol.speedDown();
+gol.turn(Directions.RIGHT);
+gol.speedDown();
+gol.stop();
+gol.openTheDoor(Doors.BEHIND_RIDE);
+gol.closeTheDoor(Doors.BEHIND_RIDE);
+gol.speedUp();
